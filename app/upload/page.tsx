7821662d083881934bd2b1ea4cd40024e3d9f340
Saied Sani <saied.sani.biderooni@gmail.com>
2023-12-09 17:15:34 +0100

    53.5- Showing Uploaded Images

--- app/upload/page.tsx
+++ app/upload/page.tsx
@@ -1,16 +1,33 @@
 "use client";
-import React from "react";
-import { CldUploadWidget } from "next-cloudinary";
+import React, { useState } from "react";
+import { CldUploadWidget, CldImage } from "next-cloudinary";
+
+interface CloudinaryRsult {
+  public_id: string;
+}
 
 const UploadPage = () => {
+  const [publicId, setPublicId] = useState("");
   return (
-    <CldUploadWidget uploadPreset="zxno3hiq">
-      {({ open }) => (
-        <button className="btn btn-primary" onClick={() => open()}>
-          Upload
-        </button>
+    <>
+      {publicId && (
+        <CldImage alt="Freedom Image" src={publicId} width={270} height={180} />
       )}
-    </CldUploadWidget>
+      <CldUploadWidget
+        uploadPreset="zxno3hiq"
+        onUpload={(result, widget) => {
+          if (result.event !== "success") return;
+          const info = result.info as CloudinaryRsult;
+          setPublicId(info.public_id);
+        }}
+      >
+        {({ open }) => (
+          <button className="btn btn-primary" onClick={() => open()}>
+            Upload
+          </button>
+        )}
+      </CldUploadWidget>
+    </>
   );
 };
 
