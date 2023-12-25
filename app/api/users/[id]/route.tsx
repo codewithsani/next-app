f0bd211d79202dd2c8d77abf7466c5ada4116245
Saied Sani <saied.sani.biderooni@gmail.com>
2023-12-25 21:42:13 +0100

    fix build errors

--- app/api/users/[id]/route.tsx
+++ app/api/users/[id]/route.tsx
@@ -7,7 +7,7 @@ export async function GET(
   { params }: { params: { id: string } }
 ) {
   const user = await prisma.user.findUnique({
-    where: { id: parseInt(params.id) },
+    where: { id: params.id },
   });
 
   if (!user)
@@ -26,7 +26,7 @@ export async function PUT(
     return NextResponse.json(validation.error.errors, { status: 400 });
 
   const user = await prisma.user.findUnique({
-    where: { id: parseInt(params.id) },
+    where: { id: params.id },
   });
 
   if (!user)
@@ -47,7 +47,7 @@ export async function DELETE(
   { params }: { params: { id: string } }
 ) {
   const user = await prisma.user.findUnique({
-    where: { id: parseInt(params.id) },
+    where: { id: params.id },
   });
   if (!user)
     return NextResponse.json({ error: "User not found" }, { status: 400 });
--- app/page.tsx
+++ app/page.tsx
@@ -2,14 +2,18 @@ import Image from "next/image";
 import Link from "next/link";
 import ProductCard from "./components/ProductCard";
 import { getServerSession } from "next-auth";
-import { authOptions } from "./api/auth/[...nextauth]/route";
+
 import { Metadata } from "next";
+import HeavyComponent from "./components/HeavyComponent";
+import { authOptions } from "./api/auth/authOptions";
+import { Span } from "next/dist/trace";
 
 export default async function Home() {
   const session = await getServerSession(authOptions);
   return (
     <main>
       <h1>Hello {session && <span>{session.user!.name}</span>}</h1>
+      <HeavyComponent />
       <Link href="/users">Users</Link>
       <ProductCard />
     </main>
@@ -20,10 +24,10 @@ export default async function Home() {
 //   title: "...",
 // };
 
-export async function generateMetadata(): Promise<Metadata> {
-  const product = await fetch("");
-  return {
-    title: "product.title",
-    description: "product.description",
-  };
-}
+// export async function generateMetadata(): Promise<Metadata> {
+//   const product = await fetch("");
+//   return {
+//     title: "product.title",
+//     description: "product.description",
+//   };
+// }
